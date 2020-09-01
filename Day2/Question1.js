3d56ab750072e9aa2acb40924f39307d6f069aaa
VIPIN BOHRA <60141545+VIPIN24BOHRA@users.noreply.github.com>
2020-09-01 17:54:51 +0530

    Update Question1.js

--- Day2/Question1.js
+++ Day2/Question1.js
@@ -1,1 +1,19 @@
 
+//program to search for a particular character in a string
+
+let str = "Hi I am Vipin Bohra."; // declaration of string varible with name str.
+// we use indexOf() method of string to search for a particular character or substring;
+ let index=str.indexOf('a');
+
+if (index !=-1){
+   console.log("Character 'a' is present at index : "+index); 
+  
+
+}
+
+else {
+  console.log("character 'a' is not present in a string");
+
+}
+
+
