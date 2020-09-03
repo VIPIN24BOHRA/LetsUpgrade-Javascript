8fd1d89bed5b4bd8fc46239bbc1b44a3ab7fca42
VIPIN BOHRA <60141545+VIPIN24BOHRA@users.noreply.github.com>
2020-09-03 10:04:29 +0530

    Update Question2.js

--- Day2/Question2.js
+++ Day2/Question2.js
@@ -1,1 +1,8 @@
+// Program to convert minutes into seconds
 
+
+let min = 5;  // variable that stores value of minute.
+
+let sec = min * 60;  // converting minute into seconds and then storing in sec.
+
+console.log("Total seconds for the given minutes is : " + sec); // print the second in console.
