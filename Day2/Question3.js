09ddd50064a071406a04edca81d377a8ef523029
VIPIN BOHRA <60141545+VIPIN24BOHRA@users.noreply.github.com>
2020-09-03 10:37:55 +0530

    Update Question3.js

--- Day2/Question3.js
+++ Day2/Question3.js
@@ -1,1 +1,20 @@
+// Program to search for an element in a arrry of string.
+
+let arr =["America","India","Russia","Japan","Norway"]; // array of string of 5 elements.
+let search;  // element to be searched.
+let index=-1; // store the index of element if present.
+search = "Japan";
+
+for (let i =0;i<arr.length;i++){  // transversing through array and searching element.
+    if (arr[i]===search){  // if element is present.
+    
+      console.log(search + " is present in array at index :" + i);
+        index=i;
+      break;  // no need to transverse if you find the element.
+    } 
+}
+if (index==-1){ // if element is not present.
+ console.log(search+" is not present in array"); 
+  
+}
 
