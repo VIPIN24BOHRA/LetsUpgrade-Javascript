28a7b9ac6859055aa7328059deedb655ce43f2fd
VIPIN BOHRA <60141545+VIPIN24BOHRA@users.noreply.github.com>
2020-09-03 18:10:07 +0530

    Update Question4.js

--- Day2/Question4.js
+++ Day2/Question4.js
@@ -1,1 +1,14 @@
+// Program to display only elements containing 'a' in them from an array.
 
+let arr =["Vipin","Bohra","Ramesh","prime","hello","great"]; // array of string.
+
+
+console.log("Elements that contain 'a' in them are : ");
+for (let i=0;i<arr.length;i++){  // transversing through array.
+ 
+ if(arr[i].indexOf('a')!=-1){  // check if 'a' is present.
+ 
+      console.log(arr[i]);   // print the element containing 'a'.
+ } 
+  
+}
