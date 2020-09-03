4501f271981db8f7f3e489d5953fe0184cbf6a57
VIPIN BOHRA <60141545+VIPIN24BOHRA@users.noreply.github.com>
2020-09-03 18:25:36 +0530

    Update Question5.js

--- Day2/Question5.js
+++ Day2/Question5.js
@@ -1,1 +1,15 @@
+// Program to print an array in reverse order.
 
+
+let arr =['start',2,'hello',true,[1,2,3],"end"];  // array containg the elements;
+
+
+let len = arr.length; // len stores the length of array.
+
+for (let i = len-1; i>=0;i--){  // transversing through array.
+  
+  
+ console.log(arr[i]); // printing elements in reverse order.
+  
+  
+}
