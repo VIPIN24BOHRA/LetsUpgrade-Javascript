3e57d4e3227105239c19903b48fc2b433bc76e41
VIPIN BOHRA <60141545+VIPIN24BOHRA@users.noreply.github.com>
2020-09-07 18:00:58 +0530

    Create prob1.js

---
+++ Day3-4/Question 1/prob1.js
@@ -0,0 +1,21 @@
+let image=document.getElementsByTagName('img');
+
+
+function firstImage(){
+
+  image[0].src="https://i.pinimg.com/736x/24/70/16/247016dfe91b8b0d6ff89b128cfcbae7.jpg";
+
+
+}
+
+function secondImage(){
+
+
+    image[0].src="https://sm.mashable.com/t/mashable_in/photo/default/jk-rowling-harry-potter-birthday-covera_n62h.960.jpg";
+}
+
+function thirdImage(){
+
+
+    image[0].src="https://lh3.googleusercontent.com/8iZjDOMW5B4Xuk1Qv8VtrAy-TYGRX9_YOcvoPeSLbO1Vs78SQN71aTpFH2jnFO8RQnU";
+}
